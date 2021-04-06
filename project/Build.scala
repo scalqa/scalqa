d376be6f4bf10fc33f6ca5a162a1f49d2fc37a2c
edolgy <edolgy@gmail.com>
2021-04-06 15:04:51 -0400

    Signed-off-by: edolgy <evgeny.dolgy@gmail.com>

--- project/Build.scala
+++ project/Build.scala
@@ -14,13 +14,13 @@ object Build {
     javaSource  in Compile := baseDirectory.value / "src"
   )
 
-  lazy val core    : Project = project.in(file("core"))                        .settings(commonSettings)
-  lazy val fx      : Project = project.in(file("fx"))          .dependsOn(core).settings(commonSettings,fork in run := true)
-  lazy val test    : Project = project.in(file("test"))        .dependsOn(fx)  .settings(commonSettings,fork in run := true)
-  lazy val sample  : Project = project.in(file("sample"))      .dependsOn(fx)  .settings(commonSettings,fork in run := true)
-  lazy val generate: Project = project.in(file("generate"))    .dependsOn(core).settings(commonSettings,fork in run := true).settings(cleanMerge:= _cleanMerge.value, merge:= _merge.value)
-  lazy val doc     : Project = project.in(file("generate/doc")).dependsOn(core).settings(commonSettings,fork in run := true).settings(cleanMerge:= _cleanMerge.value, merge:= _merge.value)
-  lazy val release : Project = project.in(file("generate/release"))            .settings(commonSettings)
+  lazy val core    : Project = project.in(file("core"))                         .settings(commonSettings)
+  lazy val fx      : Project = project.in(file("fx"))           .dependsOn(core).settings(commonSettings,fork in run := true)
+  lazy val test    : Project = project.in(file("test"))         .dependsOn(fx)  .settings(commonSettings,fork in run := true)
+  lazy val sample  : Project = project.in(file("sample"))       .dependsOn(fx)  .settings(commonSettings,fork in run := true)
+  lazy val generate: Project = project.in(file("generate"))     .dependsOn(core).settings(commonSettings,fork in run := true).settings(cleanMerge:= _cleanMerge.value, merge:= _merge.value)
+  lazy val docs    : Project = project.in(file("generate/docs")).dependsOn(core).settings(commonSettings,fork in run := true).settings(cleanMerge:= _cleanMerge.value, merge:= _merge.value)
+  lazy val release : Project = project.in(file("generate/release"))             .settings(commonSettings)
 
   def  _copy       = Def.taskDyn{ ( generate / Compile / run).toTask(" "+baseDirectory.value.toString) }
   def  _merge      = Def.taskDyn{ _copy.value; ( release / Compile / compile ) }
